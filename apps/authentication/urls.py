6c50a1a48c95b09c4f8485f2833bbdafc9c1884e
Andrzej Świętek <mr.andi321@gmail.com>
2024-12-04 00:46:28 +0100

    Added few views ideas for user photo upload

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
+    path('upload/', views.upload_images, name='upload_images'),
+    path('upload/success/', views.upload_success, name='upload_success'),
 ]
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from .models import UserImage
+from .storage import FileStorage
+
+@login_required
+def upload_images(request):
+    if request.method == 'POST':
+        storage = FileStorage()
+        user = request.user
+
+        for image_type in ['face', 'silhouette']:
+            images = request.FILES.getlist(f'{image_type}_images')
+            for file in images:
+                relative_path = f'user_{user.id}/{image_type}/{file.name}'
+                saved_path = storage.save_file(file, relative_path)
+
+                UserImage.objects.create(user=user, image_type=image_type, file_path=relative_path)
+
+        return redirect('upload_success')
+
+    return render(request, 'user/upload_profile_photos.html')
+
+
+@login_required
+def upload_success(request):
+    return render(
+        request,
+        'user/upload_profile_photos_success.html',
+        {'message': 'Files uploaded successfully!'}
+    )
