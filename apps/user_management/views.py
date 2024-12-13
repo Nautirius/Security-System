b724ce3be2684405def1800f4f6689a3583611e1
Andrzej Świętek <mr.andi321@gmail.com>
2024-12-13 14:15:21 +0100

    Added forms for create and update

--- apps/user_management/views.py
+++ apps/user_management/views.py
@@ -1,7 +1,12 @@
+import logging
+
 from allauth.core.internal.httpkit import redirect
 from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
+
+from apps.authentication.models import UserProfile
+from apps.user_management.forms import CreateUserManagementForm, UpdateUserManagementForm
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -17,24 +22,47 @@ def list_users(request: HttpRequest) -> HttpResponse:
 
 def create_user(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
-        pass
+        form = CreateUserManagementForm(request.POST)
+        try:
+            if form.is_valid():
+                form.save(request)
+                return redirect('list_users')
+
+            return render(
+                request,
+                'user_management/create_user.html',
+                {'form': form}
+            )
+        except Exception as e:
+            logging.error(e)
+            return render(request, 'user_management/create_user.html', {'form': form})
     else:
         return render(request, 'user_management/create_user.html')
 
-def edit_user(request: HttpRequest, pk) -> HttpResponse:
+def edit_user(request: HttpRequest, pk: int) -> HttpResponse:
     user = get_object_or_404(User, pk=pk)
     if request.method == 'POST':
-        pass
+        form = UpdateUserManagementForm(request.POST, user=user)
+        if form.is_valid():
+            form.save(user)
+            return redirect('list_users')
+
+        return render(
+            request,
+            'user_management/update_user.html',
+            {'form': form}
+        )
     else:
-        return render(request, 'user_management/update_user.html')
+        form = UpdateUserManagementForm(user=user)
+        return render(request, 'user_management/update_user.html', {'form': form, 'user_id': pk})
 
 
-def delete_user(request: HttpRequest, pk) -> HttpResponse:
+def delete_user(request: HttpRequest, pk: int) -> HttpResponse:
     user = get_object_or_404(User, pk=pk)
     if user:
         user.delete()
     return redirect('list_users')
 
 
-def user_by_id(request: HttpRequest, pk) -> HttpResponse:
+def user_by_id(request: HttpRequest, pk: int) -> HttpResponse:
     return redirect('list_users') # TODO:
